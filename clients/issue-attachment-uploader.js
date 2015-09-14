abec7cc44dad8fe23354e403e55669abce97e452
Ivan Pecharitsa <ivanp@compatibl.com>
2015-09-14 18:18:45 +0300

    remove unused files

--- clients/issue-attachment-uploader.js
+++
@@ -1,8 +0,0 @@
-var wrapPromise = require('./../utils/promise-wrapper');
-var AttachmentUploader = module.exports = function() {
-
-};
-AttachmentUploader.prototype.uploadAttachments = function(attachmentDirPath, jira) {
-
-};
-
